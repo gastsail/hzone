9904d86352d2c59ee3cd3cae4cc68230b13dd703
gastonsaillen <gsaillen95@gmail.com>
2023-12-05 12:32:36 -0300

    removed unused import

--- hzone/build.gradle.kts
+++ hzone/build.gradle.kts
@@ -50,7 +50,7 @@ project.afterEvaluate {
             register<MavenPublication>("release") {
                 groupId = "com.github.gastsail"
                 artifactId = "hzone"
-                version = "0.9-alpha11"
+                version = "0.9-alpha12"
             }
             repositories {
                 mavenLocal()
