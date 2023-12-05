1227425568e169898abd9d2cf2dbca438ad6cfba
gastonsaillen <gsaillen95@gmail.com>
2023-12-05 12:16:13 -0300

    removed unused dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 }
 
 dependencies {
-    implementation("com.github.gastsail:hzone:0.9-alpha09")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.1")
