32ae36e8d3cf443396c2c2755e7ac6353c96241e
gastonsaillen <gsaillen95@gmail.com>
2023-12-06 19:25:35 -0300

    version 1.0.2

--- hzone/build.gradle.kts
+++ hzone/build.gradle.kts
@@ -50,7 +50,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.gastsail"
                 artifactId = "hzone"
-                version = "1.0.1"
+                version = "1.0.2"
             }
             repositories {
                 mavenLocal()
